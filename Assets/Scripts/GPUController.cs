611260f47b9d2dc688fc0ffd406ab12c8c046249
ParubokMotorniy <oles.yarish@gmail.com>
2024-03-31 01:27:37 +0200

    Meeting changes in place. Gotta modify integration and kernels to move on...

--- Assets/Scripts/GPUController.cs
+++ Assets/Scripts/GPUController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GPUController : MonoBehaviour {
 
     ComputeBuffer positionsBuffer;
+    ComputeBuffer particle0;
+    ComputeBuffer particle1;
 
     [SerializeField]
 	Material material;
@@ -13,47 +15,59 @@ public class GPUController : MonoBehaviour {
     [SerializeField]
     ComputeShader particleShader;
 
+    [SerializeField, Range(10, 200)]
+	int nParticles = 10;
+
     static readonly int
 		positionsId = Shader.PropertyToID("particlePositions"),
-		nXId = Shader.PropertyToID("nParticlesX"),
-        nYId = Shader.PropertyToID("nParticlesY"),
+		nParticlesID = Shader.PropertyToID("nParticles"),
 		stepId = Shader.PropertyToID("step"),
+        frameID = Shader.PropertyToID("frame"),
+        gravityID = Shader.PropertyToID("gravityVector"),
 		timeId = Shader.PropertyToID("time");
+
+    int frame = 0;    
 
     void UpdateTestShader()
     {
-        float step = 4f / nParticlesX;
+        float step = 4f / nParticles;
         particleShader.SetFloat(stepId, step);
         particleShader.SetFloat(timeId, Time.time);
-        particleShader.SetInt(nXId, nParticlesX);
-        particleShader.SetInt(nYId, nParticlesY);
+        particleShader.SetInt(nParticlesID, nParticles);
+        particleShader.SetInt(frameID, frame);
+        particleShader.SetVector(gravityID, Vector3.down);
 
         particleShader.SetBuffer(0, positionsId, positionsBuffer);
-        int groupsX = Mathf.CeilToInt(nParticlesX / 8f);
-        int groupsY = Mathf.CeilToInt(nParticlesY / 8f);
-		particleShader.Dispatch(0, groupsX, groupsY, 1);
+        particleShader.SetBuffer(0, "particleRead", frame % 2 == 0 ? particle0 : particle1);
+        particleShader.SetBuffer(0, "particleWrite", frame % 2 == 0 ? particle1 : particle0);
+        int nGroups = Mathf.CeilToInt(nParticles / 64f);
+		particleShader.Dispatch(0, nGroups, 1, 1);
 
         material.SetFloat(stepId, step);
         material.SetBuffer(positionsId, positionsBuffer);
 
-        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / Mathf.Min(nParticlesX, nParticlesY)));
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / nParticles));
         Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
+
+        frame++;
     }    
 
-	[SerializeField, Range(10, 200)]
-	int nParticlesX = 10;
-
-    [SerializeField, Range(10, 200)]
-	int nParticlesY = 10;
-
     void OnEnable() {
-		positionsBuffer = new ComputeBuffer(nParticlesY*nParticlesY, 3 * 4);
+		positionsBuffer = new ComputeBuffer(nParticles, 3 * 4);
+        particle0 = new ComputeBuffer(nParticles, 5 * 8);
+        particle1 = new ComputeBuffer(nParticles, 5 * 8);
 	}
 
     void OnDisable()
     {
         positionsBuffer.Release();
         positionsBuffer = null;
+
+        particle0.Release();
+        particle0 = null;
+
+        particle1.Release();
+        particle1 = null;
     }
 
 	void Update () 
