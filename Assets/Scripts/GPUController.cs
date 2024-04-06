5cb0e65808da75c5ffb4380dabf2479062294d90
ParubokMotorniy <oles.yarish@gmail.com>
2024-04-07 00:25:40 +0300

    Simplified integration and cleaned up acceleration calculation

--- Assets/Scripts/GPUController.cs
+++ Assets/Scripts/GPUController.cs
@@ -15,7 +15,7 @@ public class GPUController : MonoBehaviour {
     [SerializeField]
     ComputeShader particleShader;
 
-    [SerializeField, Range(10, 200)]
+    [SerializeField, Range(2, 200)]
 	int nParticles = 10;
 
     [SerializeField, Range(0, 10)]
@@ -25,8 +25,14 @@ public class GPUController : MonoBehaviour {
     [SerializeField, Range(0, 10)]
     float WVisch = 0;
 
+    [SerializeField, Range(0,1)]
+    float timeStep;
 
-    float boxSize = 1;
+    [SerializeField, Range(1,50)]
+    int particleMass;
+
+
+    float boxSize = 5;
     float boxCoeff = 0.001f;
 
     static readonly int
@@ -40,13 +46,15 @@ public class GPUController : MonoBehaviour {
         WPolyhID = Shader.PropertyToID("WPolyh"),
         WSpikyhID = Shader.PropertyToID("WSpikyh"),
         WVischID = Shader.PropertyToID("WVisch"),
+        timeStepID = Shader.PropertyToID("timeStep"),
+        particleMassID = Shader.PropertyToID("particleMass"),
 		timeId = Shader.PropertyToID("time");
 
     int frame = 0;    
 
     void UpdateTestShader()
     {
-        float step = 4f / nParticles;
+        float step = 10f / nParticles;
         particleShader.SetFloat(stepId, step);
         particleShader.SetFloat(timeId, Time.time);
         particleShader.SetInt(nParticlesID, nParticles);
@@ -55,11 +63,13 @@ public class GPUController : MonoBehaviour {
         particleShader.SetFloat(WPolyhID, WPolyh);
         particleShader.SetFloat(WSpikyhID, WSpikyh);
         particleShader.SetFloat(WVischID, WVisch);
+        particleShader.SetFloat(timeStepID, timeStep);
+        particleShader.SetInt(particleMassID, particleMass);
 
         particleShader.SetBuffer(0, positionsId, positionsBuffer);
         particleShader.SetBuffer(0, "particleRead", frame % 2 == 0 ? particle0 : particle1);
         particleShader.SetBuffer(0, "particleWrite", frame % 2 == 0 ? particle1 : particle0);
-        int nGroups = Mathf.CeilToInt(nParticles / 64f);
+        int nGroups = Mathf.CeilToInt(nParticles / 64.0f);
 		particleShader.Dispatch(0, nGroups, 1, 1);
 
         particleShader.SetFloat(boxSizeID, boxSize);
@@ -82,9 +92,11 @@ public class GPUController : MonoBehaviour {
     }  
 
     void OnEnable() {
-		positionsBuffer = new ComputeBuffer(nParticles, 3 * 4);
-        particle0 = new ComputeBuffer(nParticles, 5 * 8);
-        particle1 = new ComputeBuffer(nParticles, 5 * 8);
+		positionsBuffer = new ComputeBuffer(nParticles, 3 * 8);
+        particle0 = new ComputeBuffer(nParticles, 3 * 24 + 2 * 8);
+        particle1 = new ComputeBuffer(nParticles, 3 * 24 + 2 * 8);
+
+        // Debug.Break();
 	}
 
     void OnDisable()
